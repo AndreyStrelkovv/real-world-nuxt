a12a00837d96e2a7cc6178ce0c61ff719bc06480
Andrey Strelkov <andrey.strelkov@so.energy>
2022-07-22 12:00:45 +0100

    npm generate config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import EventService from "./services/EventService"
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -45,7 +47,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  // generate: {
-  //   routes: ['/event']
-  // }
+  generate: {
+    routes: () => {
+      return EventService.getEvents().then(response => {
+        return response.data.map(event => {
+          return '/event/' + event.id
+        })
+      })
+    }
+  }
 }
